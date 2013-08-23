4cf30b5960414355176e3f7ac79c9945cf00238e
Christopher Groskopf <staringmonkey@gmail.com>
2013-08-23 10:12:59 -0500

    Accept doc param.

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,18 @@
-$(function() {
-    DV.load("//www.documentcloud.org/documents/778137-lipo-charger-basic-v10.js", {
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+        results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+$(function() { 
+    var slug = getParameterByName('doc');
+
+    DV.load('//www.documentcloud.org/documents/' + slug 
++ '.js', {
         //width: 300,
         //height: 300,
-        container: "#DV-viewer"
+        container: '#DV-viewer'
     });
    
 });
