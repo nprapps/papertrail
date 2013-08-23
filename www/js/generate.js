737190faff7677cc0818824fd67e9d60a7e44690
Christopher Groskopf <staringmonkey@gmail.com>
2013-08-23 14:39:27 -0500

    Add some error handling to iframe generation.

--- www/js/generate.js
+++ www/js/generate.js
@@ -1,8 +1,19 @@
 $('#generate').click(function() {
     var url = $('input[name="doc-url"]').val();
+
+    if (url == '') {
+        alert('Please paste in a DocumentCloud URL.');
+        return;
+    }
+
     var bits = url.split('/');
     var slug = bits[bits.length - 1];
     slug = slug.substring(0, slug.length - 5);
+
+    if (bits.length < 2 || !slug) {
+        alert('This does appear to be a valid DocumentCloud URL.');
+        return;
+    }
 
     fullscreen_url = APP_CONFIG.S3_BASE_URL + '/?doc=' + slug;
     embed_url = APP_CONFIG.S3_BASE_URL + '/?embed=true&doc=' + slug;
