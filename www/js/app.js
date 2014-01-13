f234c75ddc3a024aa666d17612b1461e5eba18bc
Christopher Groskopf <staringmonkey@gmail.com>
2014-01-13 09:33:30 -0800

    Only load GA and Chartbeat in fullscreen document. Closes #40.

--- www/js/app.js
+++ www/js/app.js
@@ -42,6 +42,42 @@ function onDocumentLoad() {
     document.title = page_title;
 }
 
+function setupGoogleAnalytics() {
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', APP_CONFIG.GOOGLE_ANALYTICS_ID]);
+    _gaq.push(['_trackPageview']);
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+} 
+
+function setupChartbeat() {
+    var _sf_async_config={};
+    /** CONFIGURATION START **/
+    _sf_async_config.uid = 18888;
+    _sf_async_config.domain = "npr.org";
+    /** CONFIGURATION END **/
+    (function(){
+        function loadChartbeat() {
+            window._sf_endpt=(new Date()).getTime();
+            var e = document.createElement("script");
+            e.setAttribute("language", "javascript");
+            e.setAttribute("type", "text/javascript");
+            e.setAttribute("src",
+                (("https:" == document.location.protocol) ?
+                 "https://a248.e.akamai.net/chartbeat.download.akamai.com/102508/" :
+                 "http://static.chartbeat.com/") +
+                "js/chartbeat.js");
+            document.body.appendChild(e);
+        }
+        var oldonload = window.onload;
+        window.onload = (typeof window.onload != "function") ?
+            loadChartbeat : function() { oldonload(); loadChartbeat(); };
+    })();
+}
+
 $(function() { 
     slug = getParameterByName('id');
     embed = getParameterByName('embed') == 'true' ? true : false;
@@ -56,6 +92,9 @@ $(function() {
         $('body').addClass('embed');
     } else {
         $('body').addClass('fullscreen');
+
+        setupGoogleAnalytics();
+        setupChartbeat();
     }
 
     if (window.innerWidth <= 480) {
