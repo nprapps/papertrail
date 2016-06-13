42621f3ab84f967bd02b5f9e7c46c8c91d31d8c9
Juan Elosua <jelosua@npr.org>
2016-06-13 15:38:31 -0400

    rename template to avoid name collision and remove logging

--- www/js/generate-note.js
+++ www/js/generate-note.js
@@ -22,23 +22,22 @@ $('#generate').click(function() {
     if (match) {
         dc_slug = match[1];
         dc_note_id = match[2];
-        console.log("dc_note_id:" + dc_note_id);
-        console.log("dc_slug:" + dc_slug);
     }
     else {
         alert('This does appear to be a valid DocumentCloud Note URL.');
     }
 
     // Generate seamus HTML embed code
-    var iframe = JST.note_embed({ dc_note_id: dc_note_id, dc_slug: dc_slug});
-    $('textarea[name="embed-code"]').val(iframe);
+    var embed = JST.note({ dc_note_id: dc_note_id, dc_slug: dc_slug});
+    $('textarea[name="embed-code"]').val(embed);
     $('.results').show();
 
     /**** Generate preview ******/
 
-    // Simulate loader script
+    var bits = dc_slug.split('-');
+    var dc_slug_id = bits[0];
+    var preview_url = 'https://www.documentcloud.org/documents/' + dc_slug_id + '/annotations/' + dc_note_id + '.js'
 
-    // Load css helper function
     var loadCSS = function(url, media) {
         var link   = document.createElement('link');
         link.rel   = 'stylesheet';
@@ -49,25 +48,32 @@ $('#generate').click(function() {
         head.appendChild(link);
     };
 
-    // Load corresponding CSS
-    /*@cc_on
-    /*@if (@_jscript_version < 5.8)
-        loadCSS("//assets.documentcloud.org/note_embed/note_embed.css");
-    @else @*/
-        loadCSS("//assets.documentcloud.org/note_embed/note_embed-datauri.css");
-    /*@end
-    @*/
-
-    var bits = dc_slug.split('-');
-    var dc_slug_id = bits[0];
-    var preview_url = 'https://www.documentcloud.org/documents/' + dc_slug_id + '/annotations/' + dc_note_id + '.js'
-    console.log(preview_url);
-    $.getScript('//assets.documentcloud.org/note_embed/note_embed.js').done(function () {
-        // Wait for page load
-        $(function () {
-            //load document cloud note
+    // Simulate loader script
+    //Check if we have the CSS in place if not load it
+    if (!$("link[href='//assets.documentcloud.org/note_embed/note_embed-datauri.css']").length &&
+        !$("link[href='//assets.documentcloud.org/note_embed/note_embed.css']").length) {
+        /*@cc_on
+        /*@if (@_jscript_version < 5.8)
+            loadCSS("//assets.documentcloud.org/note_embed/note_embed.css");
+        @else @*/
+            loadCSS("//assets.documentcloud.org/note_embed/note_embed-datauri.css");
+        /*@end
+        @*/
+    }
+    if (window.dc && window.dc.embed) {
+            // Clear out existing note to force redraw
+            if (window.dc.embed.notes[dc_note_id]) {
+                delete window.dc.embed.notes[dc_note_id];
+            }
             dc.embed.loadNote(preview_url, {container: '#DC-note-example'});
+    }
+    else {
+        // Load note_embed script
+        $.getScript('//assets.documentcloud.org/note_embed/note_embed.js').done(function () {
+            $(function () {
+                dc.embed.loadNote(preview_url, {container: '#DC-note-example'});
+            });
         });
-    });
+    }
 });
 
